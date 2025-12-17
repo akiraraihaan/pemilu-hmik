9977a1f8b17b0d9a89f4c1008e122e08decd4133
akiraraihaan <raihanakirar@gmail.com>
2025-12-17 09:03:01 +0700

    remove unused backend files and models

--- src/models/Mahasiswa.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const MahasiswaSchema = new mongoose.Schema({
-  vote: { type: String, required: true },
-}, {
-  timestamps: true, // otomatis menambahkan createdAt dan updatedAt
-});
-
-export default mongoose.models.vote || mongoose.model('vote', MahasiswaSchema);
--- src/models/checktoken.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const checktokenSchema = new mongoose.Schema({
-  nim: { type: String, required: true },
-}, {
-  timestamps: true, // Menyimpan createdAt dan updatedAt secara otomatis
-});
-
-export default mongoose.models.checktoken || mongoose.model('checktoken', checktokenSchema);
